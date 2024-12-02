4074e9142df219b1650c3002498865cda931f6f8
student-2 <student-2@Apple126s-iMac.local>
2024-12-02 14:58:05 +0530

    folder uploaded

---
+++ Home VueUITests/Home_VueUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Home_VueUITestsLaunchTests.swift
+//  Home VueUITests
+//
+//  Created by student-2 on 20/11/24.
+//
+
+import XCTest
+
+final class Home_VueUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
